a64e8d30465da5f97cc99ce9ed432f9b3dd8a062
Micaela Chans <micaelachansmoure@gmail.com>
2023-09-13 13:52:36 -0300

    SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,89 +1,91 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Sidebar() {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-	// const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  // const [isDarkMode, setIsDarkMode] = useState(false);
 
-	const toggleSidebar = () => {
-		setIsSidebarOpen(!isSidebarOpen);
-	};
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-	// const toggleDarkMode = () => {
-	// 	setIsDarkMode(!isDarkMode);
-	// };
+  // const toggleDarkMode = () => {
+  // 	setIsDarkMode(!isDarkMode);
+  // };
 
-	return (
-		<nav className={`sidebar ${isSidebarOpen ? 'close' : ''}`}>
-			<header>
-				<div className="image-text">
-					<span className="image">
-						Logo
-						{/*<img src="logo.png" alt="">*/}
-					</span>
+  return (
+    <nav className={`sidebar ${isSidebarOpen ? "close" : ""}`}>
+      <header>
+        <div className="image-text">
+          <span className="image">
+            <img
+              src="https://media.discordapp.net/attachments/834507105087782992/1150776993336528946/a82d91a7e0472978d6a0e9.png?width=676&height=676"
+              alt=""
+            />
+          </span>
 
-					<div className="text logo-text">
-						<span className="name">Drachen ©</span>
-						<span className="profession">Admin Panel</span>
-					</div>
-				</div>
+          <div className="text logo-text">
+            <span className="name">Drachen ©</span>
+            <span className="profession">Admin Panel</span>
+          </div>
+        </div>
 
-				<i className="bx bx-chevron-right toggle" onClick={toggleSidebar}></i>
-			</header>
+        <i className="bx bx-chevron-right toggle" onClick={toggleSidebar}></i>
+      </header>
 
-			<div className="menu-bar">
-				<div className="menu">
-					<li className="search-box">
-						<i className="bx bx-search icon"></i>
-						<input type="text" placeholder="Search..." />
-					</li>
+      <div className="menu-bar">
+        <div className="menu">
+          <li className="search-box">
+            <i className="bx bx-search icon"></i>
+            <input type="text" placeholder="Search..." />
+          </li>
 
-					<ul className="menu-links">
-						<li className="nav-link">
-							<a href="#">
-								<i className="bx bx-home-alt icon"></i>
-								<span className="text nav-text">Dashboard</span>
-							</a>
-						</li>
+          <ul className="menu-links">
+            <li className="nav-link">
+              <a href="#">
+                <i className="bx bx-home-alt icon"></i>
+                <span className="text nav-text">Dashboard</span>
+              </a>
+            </li>
 
-						<li className="nav-link">
-							<a href="#">
-								<i className="bx bx-bar-chart-alt-2 icon"></i>
-								<span className="text nav-text">Revenue</span>
-							</a>
-						</li>
+            <li className="nav-link">
+              <a href="#">
+                <i className="bx bx-bar-chart-alt-2 icon"></i>
+                <span className="text nav-text">Revenue</span>
+              </a>
+            </li>
 
-						<li className="nav-link">
-							<a href="#">
-								<i className="bx bx-bell icon bx-tada-hover"></i>
-								<span className="text nav-text">Notifications</span>
-							</a>
-						</li>
+            <li className="nav-link">
+              <a href="#">
+                <i className="bx bx-bell icon bx-tada-hover"></i>
+                <span className="text nav-text">Notifications</span>
+              </a>
+            </li>
 
-						<li className="nav-link">
-							<a href="#">
-								<i className="bx bx-pie-chart-alt icon"></i>
-								<span className="text nav-text">Analytics</span>
-							</a>
-						</li>
+            <li className="nav-link">
+              <a href="#">
+                <i className="bx bx-pie-chart-alt icon"></i>
+                <span className="text nav-text">Analytics</span>
+              </a>
+            </li>
 
-						<li className="nav-link">
-							<a href="#">
-								<i className="bx bx-package icon"></i>
-								<span className="text nav-text">Orders</span>
-							</a>
-						</li>
-					</ul>
-				</div>
+            <li className="nav-link">
+              <a href="#">
+                <i className="bx bx-package icon"></i>
+                <span className="text nav-text">Orders</span>
+              </a>
+            </li>
+          </ul>
+        </div>
 
-				<div className="bottom-content">
-					<li className="">
-						<a href="#">
-							<i className="bx bx-log-out icon"></i>
-							<span className="text nav-text">Logout</span>
-						</a>
-					</li>
+        <div className="bottom-content">
+          <li className="">
+            <a href="#">
+              <i className="bx bx-log-out icon"></i>
+              <span className="text nav-text">Logout</span>
+            </a>
+          </li>
 
-					{/* <li className="mode" onClick={toggleDarkMode}>
+          {/* <li className="mode" onClick={toggleDarkMode}>
 						<div className="sun-moon">
 							<i className={`bx ${isDarkMode ? 'bx-sun' : 'bx-moon'} icon moon`}></i>
 							<i className={`bx ${isDarkMode ? 'bx-sun' : 'bx-moon'} icon sun`}></i>
@@ -96,10 +98,10 @@ function Sidebar() {
 							></span>
 						</div>
 					</li> */}
-				</div>
-			</div>
-		</nav>
-	);
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Sidebar;
